241f1e275e4785cd0afbe5ce6ccdff4e3099b5ad
Aleksey <fomichaaf.95@gmail.com>
2023-06-25 21:37:26 +0300

    create console app

---
+++ TestProject.cs
@@ -0,0 +1,9 @@
+using System;
+public class Test
+{
+    public static void GetGreetings()
+    {
+        string name = Console.ReadLine();
+        Console.WriteLine("Приветствую, {0}!!! \nДобро пожаловать в консольное приложение", name);
+    }
+}
