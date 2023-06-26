17688fa137498bb6c428a7f938de8e8d379c3fd1
Aleksey <fomichaaf.95@gmail.com>
2023-06-26 16:35:56 +0300

    add file gitIgnore

--- TestProject.cs
+++ TestProject.cs
@@ -4,6 +4,6 @@ public class Test
     public static void GetGreetings()
     {
         string name = Console.ReadLine();
-        Console.WriteLine("Приветствую, {0}!!! \nДобро пожаловать в консольное приложение", name);
+        Console.WriteLine("Приветствую, {0}!!!\nДобро пожаловать в консольное приложение", name);
     }
 }
